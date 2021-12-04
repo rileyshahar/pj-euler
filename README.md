468f7e01e28ed3d041823e113fff137f9b388e90
Riley Shahar <riley.shahar@gmail.com>
2021-12-04 15:32:32 -0800

    Implement problem 1

---
+++ README.md
@@ -0,0 +1,3 @@
+My Rust solutions to the [Project Euler](https://projecteuler.net/) problems.
+
+Benchmark with `cargo bench`. Run with `cargo run <problem-num>`.
---
+++ main/src/main.rs
@@ -0,0 +1,34 @@
+#![feature(test)]
+
+/// Generate a main function which relies on `solve` and a benchmark.
+macro_rules! euler_problems {
+    ( $($num:pat => $name:ident)* ) => {
+        fn main() {
+            // really simple CLI arg parser to get the first arg and check it against problems
+            let solution = match std::env::args().skip(1).next().unwrap_or_default().as_str() {
+                $(
+                    $num | stringify!($name) => $name::solve(),
+                )*
+                unknown => panic!("no matching problem: {}", unknown)
+            };
+            println!("{}", solution);
+        }
+
+        #[cfg(test)]
+        mod benches {
+            extern crate test;
+            use test::Bencher;
+
+            $(
+                #[bench]
+                fn $name(b: &mut Bencher) {
+                    b.iter(|| $name::solve());
+                }
+            )*
+        }
+    };
+}
+
+euler_problems! {
+    "1" => divide_three_five
+}
